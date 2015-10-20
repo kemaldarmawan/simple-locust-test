e6a7e07f342111b82d81369381edd8175c00cdec
Kemal Darmawan <kemal@traveloka.com>
2015-10-20 14:58:25 +0700

    add on_start method for task

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,7 @@
 import yaml
 from locust import HttpLocust, TaskSet
 
-config = yaml.load(file('api.yml'))
+config = yaml.load(file('api.yaml'))
 
 
 def create_task(request):
@@ -29,6 +29,13 @@ def load_task():
 
 
 class UserBehavior(TaskSet):
+    def on_start(self):
+        if 'on_start' in config:
+            for request in config['on_start']:
+                print 'hello'
+                start_task = create_task(request)
+                print type(start_task)
+                start_task(self)
     tasks = load_task()
 
 
